719da2878f31c7416af24b73a721b632e8620d17
iamjonguz <iamjonguz@gmail.com>
2022-12-30 14:44:18 +0100

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import { Home } from './views/pages/Home';
 import { Menu } from './views/pages/Menu/Menu';
 
 function App() {
   return (
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
-          <Route path="/menu-recipe-site" element={<Home />} />
-          <Route path="/menu-recipe-site/menu" element={<Menu />} />
+          <Route path="/" element={<Home />} />
+          <Route path="menu" element={<Menu />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
   );
 }
 
